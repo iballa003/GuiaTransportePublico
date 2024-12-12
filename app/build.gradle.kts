f58f0c113283f0f582606505673a9c07f7519e01
iballa003 <iballa.montero@gmail.com>
2024-12-12 12:04:53 +0000

    Instalado la api KSP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -40,6 +41,7 @@ android {
 }
 
 dependencies {
+    ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-rxjava2:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
