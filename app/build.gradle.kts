9d59014927e2bb5781700083ededd25568affa83
iballa003 <iballa.montero@gmail.com>
2024-12-12 12:02:02 +0000

    Instalado las dependencias de Room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,9 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-rxjava2:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
