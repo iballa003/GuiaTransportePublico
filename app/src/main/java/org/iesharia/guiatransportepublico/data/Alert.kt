6733b61c9954a1a9f56a9bdebba50cbe77ddc4db
iballa003 <iballa.montero@gmail.com>
2024-12-16 18:48:22 +0000

    Creada la relación entre alert y road

--- app/src/main/java/org/iesharia/guiatransportepublico/data/Alert.kt
+++ app/src/main/java/org/iesharia/guiatransportepublico/data/Alert.kt
@@ -3,7 +3,14 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(
+    foreignKeys = [ForeignKey(
+        entity = Road::class,
+        parentColumns = ["id"],
+        childColumns = ["road_id"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
 data class Alert(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String,
