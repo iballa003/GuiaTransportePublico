a7227a187e49bece3d36a009ccda8578d8cb240e
iballa003 <iballa.montero@gmail.com>
2024-12-16 22:45:45 +0000

    Modificado StopViewModel.kt

--- app/src/main/java/org/iesharia/guiatransportepublico/ui/StopViewModel.kt
+++ app/src/main/java/org/iesharia/guiatransportepublico/ui/StopViewModel.kt
@@ -1,2 +1,18 @@
 package org.iesharia.guiatransportepublico.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.iesharia.guiatransportepublico.data.Stop
+
+class StopViewModel(private val repository: MarkerRepository) : ViewModel() {
+    val allMarkers: LiveData<List<Stop>> = repository.allMarkers
+    fun insertData(markerTypes: List<Stop>, markers: List<Stop>) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertData(markerTypes, markers)
+        }
+    }
+
+}
