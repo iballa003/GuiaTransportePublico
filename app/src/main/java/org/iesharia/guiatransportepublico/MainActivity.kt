1737c13c8a6ddfbd44acf40578c78f8df2cf3bec
iballa003 <iballa.montero@gmail.com>
2024-12-16 23:27:58 +0000

    Modificado el MainActivity.kt para que envíe el ViewModel al HomeScreen.kt

--- app/src/main/java/org/iesharia/guiatransportepublico/MainActivity.kt
+++ app/src/main/java/org/iesharia/guiatransportepublico/MainActivity.kt
@@ -1,23 +1,40 @@
 package org.iesharia.guiatransportepublico
 
+import MyMapView
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
 import org.iesharia.guiatransportepublico.data.AppDatabase2
+import org.iesharia.guiatransportepublico.ui.StopRepository
+import org.iesharia.guiatransportepublico.ui.StopViewModel
+import org.iesharia.guiatransportepublico.ui.StopViewModelFactory
 import org.iesharia.guiatransportepublico.ui.theme.GuiaTransportePublicoTheme
 
 class MainActivity : ComponentActivity() {
     private lateinit var database: AppDatabase2
+    private val stopViewModel: StopViewModel by viewModels {
+        StopViewModelFactory(StopRepository(AppDatabase2.getDatabase(this).guideDao()))
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         database = AppDatabase2.getDatabase(this)
         enableEdgeToEdge()
         setContent {
             GuiaTransportePublicoTheme {
-
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MyMapView(
+                        modifier = Modifier.padding(innerPadding),
+                        database,
+                        stopViewModel)
+                }
+            }
         }
     }
-}
 }
 
