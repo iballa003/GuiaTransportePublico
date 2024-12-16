4a3e9df5abfc97d77df8b3801262d573538c1f28
iballa003 <iballa.montero@gmail.com>
2024-12-16 12:14:36 +0000

    MOdificado el MainActivity.kt para obtener la base de datos.

--- app/src/main/java/org/iesharia/guiatransportepublico/MainActivity.kt
+++ app/src/main/java/org/iesharia/guiatransportepublico/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import org.iesharia.guiatransportepublico.data.AppDatabase2
 import org.iesharia.guiatransportepublico.ui.theme.GuiaTransportePublicoTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var database: AppDatabase2
     override fun onCreate(savedInstanceState: Bundle?) {
-        private lateinit var database: AppDatabase
         super.onCreate(savedInstanceState)
+        database = AppDatabase2.getDatabase(this)
         enableEdgeToEdge()
         setContent {
             GuiaTransportePublicoTheme {
