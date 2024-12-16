863ba9ebaf06291bbebd1422fc6e1efeccefcd78
iballa003 <iballa.montero@gmail.com>
2024-12-16 18:36:34 +0000

    Modificado los campos de Stop.kt

--- app/src/main/java/org/iesharia/guiatransportepublico/data/Stop.kt
+++ app/src/main/java/org/iesharia/guiatransportepublico/data/Stop.kt
@@ -2,11 +2,18 @@ package org.iesharia.guiatransportepublico.data
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
-
+@Entity(
+    foreignKeys = [ForeignKey(
+        entity = Road::class,
+        parentColumns = ["id"],
+        childColumns = ["ruta_id"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
 data class Stop(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val title: String,
+    val nombre: String,
     val latitude: Double,
     val longitude: Double,
-    val typeId: Int
+    val ruta_id: Int
 )
