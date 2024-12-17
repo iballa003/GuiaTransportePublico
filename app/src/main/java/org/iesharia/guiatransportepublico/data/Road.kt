cc9766bf03493ca1225f72f159367b9cb4da5e5a
iballa003 <iballa.montero@gmail.com>
2024-12-17 00:43:23 +0000

    Corregido la descripción que estaba en Double

--- app/src/main/java/org/iesharia/guiatransportepublico/data/Road.kt
+++ app/src/main/java/org/iesharia/guiatransportepublico/data/Road.kt
@@ -7,5 +7,5 @@ import androidx.room.PrimaryKey
 data class Road(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String,
-    val description: Double,
+    val description: String,
 )
