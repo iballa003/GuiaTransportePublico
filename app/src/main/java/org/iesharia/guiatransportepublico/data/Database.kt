e96de3a18e0aca2f49d5b9359db939062d86b5c4
iballa003 <iballa.montero@gmail.com>
2024-12-17 10:36:55 +0000

    Cambiado la version de la base de datos debido un cambio en el esquema

--- app/src/main/java/org/iesharia/guiatransportepublico/data/Database.kt
+++ app/src/main/java/org/iesharia/guiatransportepublico/data/Database.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Road::class, Stop::class], version = 1)
+@Database(entities = [Road::class, Stop::class], version = 2)
 abstract class AppDatabase2 : RoomDatabase() {
     abstract fun guideDao(): GuideDao
 
