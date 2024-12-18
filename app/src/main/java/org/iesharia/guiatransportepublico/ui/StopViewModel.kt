069ac7ac557547524b447b3323c80a219c887334
iballa003 <iballa.montero@gmail.com>
2024-12-18 09:14:55 +0000

    Quitado más imports innecesarios.

--- app/src/main/java/org/iesharia/guiatransportepublico/ui/StopViewModel.kt
+++ app/src/main/java/org/iesharia/guiatransportepublico/ui/StopViewModel.kt
@@ -1,21 +1,16 @@
 package org.iesharia.guiatransportepublico.ui
 
-import android.app.Application
+
 import android.content.Context
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.asFlow
 import androidx.lifecycle.viewModelScope
-import androidx.room.Database
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import org.iesharia.guiatransportepublico.data.AppDatabase2
-import org.iesharia.guiatransportepublico.data.GuideDao
 import org.iesharia.guiatransportepublico.data.Road
 import org.iesharia.guiatransportepublico.data.Stop
 import org.iesharia.guiatransportepublico.data.StopWithRoute
