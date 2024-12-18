246f688551df85b65a824ea88ef111722e618b00
iballa003 <iballa.montero@gmail.com>
2024-12-18 08:49:16 +0000

    Arreglado un fallo con los datos iniciales de la base de datos.

--- app/src/main/java/org/iesharia/guiatransportepublico/data/Database.kt
+++ app/src/main/java/org/iesharia/guiatransportepublico/data/Database.kt
@@ -1,5 +1,6 @@
 package org.iesharia.guiatransportepublico.data
 
+import android.annotation.SuppressLint
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
@@ -29,6 +30,7 @@ abstract class AppDatabase2 : RoomDatabase() {
         }
     }
     private class DatabaseCallback(private val context: Context) : RoomDatabase.Callback() {
+        @SuppressLint("SuspiciousIndentation")
         override fun onCreate(db: SupportSQLiteDatabase) {
             super.onCreate(db)
             // Insertar datos iniciales cuando la base de datos se crea por primera vez
@@ -38,27 +40,32 @@ abstract class AppDatabase2 : RoomDatabase() {
 
                 // Crear datos iniciales
                 if (guideDao.getCountRoads() == 0) { // Verificar si está vacía
-                val roads = listOf(
-                    Road(id = 1, name = "Arrecife-Orzola", "De Arrecife a Orzola"),
-                    Road(id = 2, name = "Arrecife-Maguez", "De Arrecife a Maguez")
-                )
-                    // Insertar datos
-                    guideDao.insertAllRoads(roads)
+                    val roads = listOf(
+                        Road(id = 1, name = "Arrecife-Orzola", "De Arrecife a Orzola"),
+                        Road(id = 2, name = "Arrecife-Maguez", "De Arrecife a Maguez"),
+                        Road(id = 3, name = "Arrecife-Yé", "De Arrecife a Yé"),
+                    )
+                        // Insertar datos
+                        guideDao.insertAllRoads(roads)
                 }
                 if (guideDao.getCountStops() == 0) { // Verificar si está vacía
                     val stops = listOf(
                         Stop(
-                            id = 1,
-                            name = "Parada 1",
+                            name = "Parada centro de salud de Mala",
                             latitude = 28.957473,
                             longitude = -13.554514,
                             road_id = 1
                         ),
                         Stop(
-                            id = 2,
-                            name = "Parada 2",
+                            name = "Parada Jardín de Cactús",
                             latitude = 28.954321,
                             longitude = -13.551234,
+                            road_id = 2
+                        ),
+                        Stop(
+                            name = "Estación de guaguas",
+                            latitude = 28.967133068307646,
+                            longitude = -13.551609596110522,
                             road_id = 2
                         )
                     )
